9e548118d85c9e0d034a1b7bd752744cdb2cb8eb
Kashyap Bhansali <kashyapbhansali7@gmail.com>
2016-09-05 16:59:02 -0700

    Adding DataViz assignment from Adaptive Web
    Stack Overflow Java Data set.  (Social , Interactive Visualizations)

---
+++ DataVizStackOverflowJava/opsbats.js
@@ -0,0 +1,560 @@
+var ops = [
+  {
+    "u_id": "A0042",
+    "count": 34,
+    "sort": 18,
+    "array": 3,
+    "string": 2,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0034",
+    "count": 8,
+    "sort": 0,
+    "array": 3,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0041",
+    "count": 124,
+    "sort": 10,
+    "array": 17,
+    "string": 17,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0048",
+    "count": 158,
+    "sort": 11,
+    "array": 43,
+    "string": 14,
+    "class": 5,
+    "loop": 9
+  },
+  {
+    "u_id": "A0044",
+    "count": 55,
+    "sort": 5,
+    "array": 12,
+    "string": 6,
+    "class": 2,
+    "loop": 0
+  },
+  {
+    "u_id": "A0029",
+    "count": 84,
+    "sort": 8,
+    "array": 15,
+    "string": 21,
+    "class": 6,
+    "loop": 0
+  },
+  {
+    "u_id": "A0066",
+    "count": 306,
+    "sort": 4,
+    "array": 35,
+    "string": 21,
+    "class": 33,
+    "loop": 9
+  },
+  {
+    "u_id": "A0043",
+    "count": 20,
+    "sort": 2,
+    "array": 7,
+    "string": 1,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0039",
+    "count": 85,
+    "sort": 0,
+    "array": 18,
+    "string": 2,
+    "class": 6,
+    "loop": 2
+  },
+  {
+    "u_id": "A0071",
+    "count": 12,
+    "sort": 1,
+    "array": 9,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0084",
+    "count": 112,
+    "sort": 5,
+    "array": 5,
+    "string": 12,
+    "class": 34,
+    "loop": 2
+  },
+  {
+    "u_id": "A0020",
+    "count": 56,
+    "sort": 2,
+    "array": 27,
+    "string": 2,
+    "class": 2,
+    "loop": 1
+  },
+  {
+    "u_id": "A0067",
+    "count": 38,
+    "sort": 5,
+    "array": 16,
+    "string": 0,
+    "class": 0,
+    "loop": 1
+  },
+  {
+    "u_id": "A0025",
+    "count": 111,
+    "sort": 1,
+    "array": 18,
+    "string": 9,
+    "class": 12,
+    "loop": 3
+  },
+  {
+    "u_id": "A0026",
+    "count": 15,
+    "sort": 2,
+    "array": 1,
+    "string": 1,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0073",
+    "count": 59,
+    "sort": 5,
+    "array": 17,
+    "string": 1,
+    "class": 7,
+    "loop": 4
+  },
+  {
+    "u_id": "A0011",
+    "count": 132,
+    "sort": 3,
+    "array": 11,
+    "string": 25,
+    "class": 17,
+    "loop": 0
+  },
+  {
+    "u_id": "B0025",
+    "count": 35,
+    "sort": 0,
+    "array": 6,
+    "string": 1,
+    "class": 8,
+    "loop": 0
+  },
+  {
+    "u_id": "B0005",
+    "count": 65,
+    "sort": 0,
+    "array": 7,
+    "string": 3,
+    "class": 2,
+    "loop": 4
+  },
+  {
+    "u_id": "A0015",
+    "count": 2,
+    "sort": 0,
+    "array": 0,
+    "string": 1,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0009",
+    "count": 96,
+    "sort": 9,
+    "array": 15,
+    "string": 3,
+    "class": 6,
+    "loop": 0
+  },
+  {
+    "u_id": "A0074",
+    "count": 280,
+    "sort": 24,
+    "array": 23,
+    "string": 40,
+    "class": 14,
+    "loop": 4
+  },
+  {
+    "u_id": "A0075",
+    "count": 7,
+    "sort": 7,
+    "array": 4,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0087",
+    "count": 8,
+    "sort": 6,
+    "array": 0,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0059",
+    "count": 39,
+    "sort": 5,
+    "array": 7,
+    "string": 3,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0047",
+    "count": 132,
+    "sort": 0,
+    "array": 9,
+    "string": 19,
+    "class": 12,
+    "loop": 3
+  },
+  {
+    "u_id": "B0013",
+    "count": 13,
+    "sort": 0,
+    "array": 0,
+    "string": 2,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0065",
+    "count": 32,
+    "sort": 3,
+    "array": 2,
+    "string": 6,
+    "class": 2,
+    "loop": 0
+  },
+  {
+    "u_id": "A0033",
+    "count": 29,
+    "sort": 0,
+    "array": 6,
+    "string": 1,
+    "class": 4,
+    "loop": 0
+  },
+  {
+    "u_id": "B0007",
+    "count": 81,
+    "sort": 1,
+    "array": 5,
+    "string": 10,
+    "class": 8,
+    "loop": 10
+  },
+  {
+    "u_id": "A0054",
+    "count": 99,
+    "sort": 1,
+    "array": 12,
+    "string": 8,
+    "class": 10,
+    "loop": 0
+  },
+  {
+    "u_id": "A0030",
+    "count": 4,
+    "sort": 0,
+    "array": 0,
+    "string": 0,
+    "class": 4,
+    "loop": 0
+  },
+  {
+    "u_id": "A0088",
+    "count": 22,
+    "sort": 0,
+    "array": 2,
+    "string": 1,
+    "class": 2,
+    "loop": 0
+  },
+  {
+    "u_id": "A0005",
+    "count": 70,
+    "sort": 2,
+    "array": 8,
+    "string": 6,
+    "class": 2,
+    "loop": 0
+  },
+  {
+    "u_id": "A0001",
+    "count": 44,
+    "sort": 0,
+    "array": 0,
+    "string": 1,
+    "class": 3,
+    "loop": 1
+  },
+  {
+    "u_id": "A0077",
+    "count": 24,
+    "sort": 21,
+    "array": 4,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0082",
+    "count": 50,
+    "sort": 1,
+    "array": 3,
+    "string": 8,
+    "class": 2,
+    "loop": 2
+  },
+  {
+    "u_id": "A0010",
+    "count": 127,
+    "sort": 9,
+    "array": 21,
+    "string": 6,
+    "class": 15,
+    "loop": 2
+  },
+  {
+    "u_id": "A0064",
+    "count": 57,
+    "sort": 8,
+    "array": 26,
+    "string": 18,
+    "class": 0,
+    "loop": 3
+  },
+  {
+    "u_id": "A0022",
+    "count": 20,
+    "sort": 2,
+    "array": 9,
+    "string": 1,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0050",
+    "count": 89,
+    "sort": 0,
+    "array": 20,
+    "string": 14,
+    "class": 0,
+    "loop": 13
+  },
+  {
+    "u_id": "A0061",
+    "count": 43,
+    "sort": 5,
+    "array": 10,
+    "string": 0,
+    "class": 2,
+    "loop": 0
+  },
+  {
+    "u_id": "A0040",
+    "count": 9,
+    "sort": 6,
+    "array": 0,
+    "string": 0,
+    "class": 1,
+    "loop": 0
+  },
+  {
+    "u_id": "A0021",
+    "count": 46,
+    "sort": 0,
+    "array": 0,
+    "string": 1,
+    "class": 3,
+    "loop": 0
+  },
+  {
+    "u_id": "A0046",
+    "count": 393,
+    "sort": 9,
+    "array": 51,
+    "string": 46,
+    "class": 49,
+    "loop": 11
+  },
+  {
+    "u_id": "A0057",
+    "count": 51,
+    "sort": 12,
+    "array": 20,
+    "string": 6,
+    "class": 2,
+    "loop": 1
+  },
+  {
+    "u_id": "A0024",
+    "count": 46,
+    "sort": 11,
+    "array": 15,
+    "string": 2,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0049",
+    "count": 71,
+    "sort": 6,
+    "array": 14,
+    "string": 6,
+    "class": 6,
+    "loop": 1
+  },
+  {
+    "u_id": "A0056",
+    "count": 35,
+    "sort": 2,
+    "array": 14,
+    "string": 0,
+    "class": 1,
+    "loop": 2
+  },
+  {
+    "u_id": "B0026",
+    "count": 37,
+    "sort": 0,
+    "array": 3,
+    "string": 2,
+    "class": 19,
+    "loop": 0
+  },
+  {
+    "u_id": "A0069",
+    "count": 8,
+    "sort": 8,
+    "array": 2,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "A0019",
+    "count": 66,
+    "sort": 5,
+    "array": 11,
+    "string": 15,
+    "class": 3,
+    "loop": 2
+  },
+  {
+    "u_id": "A0052",
+    "count": 38,
+    "sort": 10,
+    "array": 6,
+    "string": 0,
+    "class": 3,
+    "loop": 0
+  },
+  {
+    "u_id": "B0012",
+    "count": 35,
+    "sort": 1,
+    "array": 3,
+    "string": 4,
+    "class": 1,
+    "loop": 2
+  },
+  {
+    "u_id": "B0020",
+    "count": 81,
+    "sort": 10,
+    "array": 13,
+    "string": 7,
+    "class": 8,
+    "loop": 0
+  },
+  {
+    "u_id": "A0080",
+    "count": 97,
+    "sort": 6,
+    "array": 25,
+    "string": 5,
+    "class": 6,
+    "loop": 0
+  },
+  {
+    "u_id": "B0027",
+    "count": 54,
+    "sort": 5,
+    "array": 10,
+    "string": 13,
+    "class": 2,
+    "loop": 4
+  },
+  {
+    "u_id": "A0018",
+    "count": 21,
+    "sort": 11,
+    "array": 1,
+    "string": 4,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "B0018",
+    "count": 33,
+    "sort": 0,
+    "array": 0,
+    "string": 6,
+    "class": 3,
+    "loop": 0
+  },
+  {
+    "u_id": "A0008",
+    "count": 73,
+    "sort": 0,
+    "array": 2,
+    "string": 6,
+    "class": 5,
+    "loop": 6
+  },
+  {
+    "u_id": "A0081",
+    "count": 14,
+    "sort": 7,
+    "array": 11,
+    "string": 0,
+    "class": 0,
+    "loop": 0
+  },
+  {
+    "u_id": "B0021",
+    "count": 11,
+    "sort": 2,
+    "array": 5,
+    "string": 1,
+    "class": 0,
+    "loop": 0
+  }
+];
